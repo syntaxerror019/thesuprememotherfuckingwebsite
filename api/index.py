3d110c8918620c7aff34164b87bc512856ef9041
mhilliard <142847239+syntaxerror019@users.noreply.github.com>
2025-03-06 13:18:55 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,10 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+res = """
+THE SUPREME MOTHERFUCKING WEBSITE
+---------------------------------
+With all your fancy template based websites you probably think markdown is where its at.
+Have you forgotten?
+
+"""
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+    return ''
