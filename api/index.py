a7b9c399e5948feb539cec49d8058863d46ca3c9
mhilliard <142847239+syntaxerror019@users.noreply.github.com>
2025-03-07 07:26:48 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,14 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-res = """
-THE SUPREME MOTHERFUCKING WEBSITE\n
----------------------------------\n
-With all your fancy template based websites you probably think markdown is where its at.\n
-Have you forgotten?
-
-"""
-
 @app.route('/')
 def home():
+    with open("index.txt", "r") as f:
+        res = file.readlines()
     return res
