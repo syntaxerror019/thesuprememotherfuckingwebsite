53d69cfb3895a2029a1c731846193f6b598c41bb
mhilliard <142847239+syntaxerror019@users.noreply.github.com>
2025-03-07 07:23:27 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 res = """
-THE SUPREME MOTHERFUCKING WEBSITE
----------------------------------
-With all your fancy template based websites you probably think markdown is where its at.
+THE SUPREME MOTHERFUCKING WEBSITE\n
+---------------------------------\n
+With all your fancy template based websites you probably think markdown is where its at.\n
 Have you forgotten?
 
 """
